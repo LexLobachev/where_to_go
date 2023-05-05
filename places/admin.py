b1e0f6077d6721ce587a60d8078bfbb240562546
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-05 20:10:21 +0400

    add preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInline(admin.StackedInline):
     model = Image
     exta = 0
+
+    def get_place_images(self, object):
+        return format_html(f'<img src="{object.image.url}" height=200px />')
+
+    readonly_fields = ['get_place_images']
 
 
 @admin.register(Place)
