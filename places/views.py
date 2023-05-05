438e2c0944a92708fc08a949e1de0b5ed15efba3
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-05 13:52:58 +0400

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
+from places.models import Place
+
 
 def index(request):
-    return render(request, 'index.html')
+    places = []
+    for place in Place.objects.all():
+        place = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    place.lon,
+                    place.lat
+                ]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.pk,
+                "detailsUrl": f"/static/places/roofs24.json"
+            }
+        }
+        places.append(place)
+    collection = {
+        "type": "FeatureCollection",
+        "features": places
+    }
+    print(collection)
+    return render(request, 'index.html', context={"geo_json": collection})
