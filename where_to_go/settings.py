68ca017692fb2887dca437f696bdb91e9a57b817
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-07 13:40:23 +0400

    fix static problem

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, 'assets'))
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
