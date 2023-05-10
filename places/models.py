659a21ec5e86ee7e767bff1bd52729faeef9bf90
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-10 19:32:17 +0400

    change models

--- places/models.py
+++ places/models.py
@@ -5,12 +5,12 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
     description_short = models.TextField(blank=True, verbose_name="Короткое описание")
-    description_long = HTMLField(verbose_name="Длинное описание")
+    description_long = HTMLField(blank=True, verbose_name="Длинное описание")
     lat = models.FloatField(blank=True, verbose_name="широта")
     lon = models.FloatField(blank=True, verbose_name="долгота")
 
     class Meta:
-        ordering = ['id']
+        ordering = ['title']
 
     def __str__(self):
         return self.title
@@ -18,7 +18,7 @@ class Place(models.Model):
 
 class Image(models.Model):
     place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField('Картинка', blank=True)
+    image = models.ImageField('Картинка')
     position = models.IntegerField('Номер картинки', blank=True, default=0)
 
     class Meta:
