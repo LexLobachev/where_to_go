5d0c23ba8d1d23cf771170ae399ef6c97b7145fd
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-13 11:51:39 +0400

    make field obligatory

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     description_short = models.TextField(blank=True, verbose_name='Короткое описание')
     description_long = HTMLField(blank=True, verbose_name='Длинное описание')
-    lat = models.FloatField(blank=True, verbose_name='широта')
-    lon = models.FloatField(blank=True, verbose_name='долгота')
+    lat = models.FloatField(verbose_name='широта')
+    lon = models.FloatField(verbose_name='долгота')
 
     def __str__(self):
         return self.title
